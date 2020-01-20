3284a77dd204ba0344d98d0aaaea76b34b5532ee
DovydasAL <davidlazauskas1@gmail.com>
2020-01-20 03:37:11 -0500

    add database project

---
+++ TMSDatabase/dbo/Tables/Listings.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [dbo].[Listings] (
+    [ListingID]         BIGINT        IDENTITY (1, 1) NOT NULL,
+    [Subject]           VARCHAR (255) NOT NULL,
+    [CourseNumber]      VARCHAR (255) NOT NULL,
+    [InstructionType]   VARCHAR (255) NOT NULL,
+    [InstructionMethod] VARCHAR (255) NOT NULL,
+    [Section]           VARCHAR (255) NOT NULL,
+    [CRN]               VARCHAR (255) NOT NULL,
+    [Enroll]            VARCHAR (255) NOT NULL,
+    [MaxEnroll]         VARCHAR (255) NOT NULL,
+    [CourseTitle]       VARCHAR (255) NOT NULL,
+    [Times]             VARCHAR (MAX) NOT NULL,
+    [Instructor]        VARCHAR (255) NOT NULL,
+    [TermID]            BIGINT        NOT NULL,
+    [CreatedDate]       DATETIME      NOT NULL,
+    [ModifiedDate]      DATETIME      NOT NULL
+);
+
---
+++ TMSDatabase/dbo/Tables/Terms.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Terms] (
+    [TermID]      BIGINT        IDENTITY (1, 1) NOT NULL,
+    [TermName]    VARCHAR (255) NOT NULL,
+    [LookUpLabel] VARCHAR (255) NOT NULL
+);
+
